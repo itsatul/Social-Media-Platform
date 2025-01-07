bd67c582b6ef4e9736c40404825c8794420b092c
Tiara <ht.nguyenthi@gmail.com>
2025-01-07 22:05:45 +0100

    implementing Gunicorn

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,5 +1,4 @@
 #python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-#gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
-python manage.py runserver 0:8000
+gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
