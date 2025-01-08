7468ed56251b9e651b383eec77c1fb8681f8b0d2
Tiara <ht.nguyenthi@gmail.com>
2025-01-08 11:13:17 +0100

    location changed to /backend/ for the backend

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -21,7 +21,7 @@ from django.urls import path
 from project import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("backend/admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
