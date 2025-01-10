397c44b5ccb80f9cc359e5cf5085a0b935bcf2fe
Tiara <ht.nguyenthi@gmail.com>
2025-01-10 13:55:49 +0100

    registration model with urls and views implemented

---
+++ backend/registration/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import Registration
+User = get_user_model()
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model: Registration
+        fields = ['user', 'is_validated', 'created_at']
