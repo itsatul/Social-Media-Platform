2ccc0ad93d9841de0292fff78c5b06fc57c025cb
Tiara <ht.nguyenthi@gmail.com>
2025-01-10 13:58:04 +0100

    linting

--- backend/registration/serializers.py
+++ backend/registration/serializers.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Registration
+
 User = get_user_model()
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
